313aed95fe7fcd68044ea79d9a49d35416d4c5c8
Morgan <143568735+Morclouds@users.noreply.github.com>
2024-09-27 19:19:54 -0500

    set up

---
+++ app.js
@@ -0,0 +1,7 @@
+let text = document.getElementById("title").innerHTML;
+document.getElementById("title").innerHTML =
+text.toUpperCase();
+
+const welcome = "Hi this is a"
+const item = "prime number checker"
+console.log(`${welcome} ${item}`)
